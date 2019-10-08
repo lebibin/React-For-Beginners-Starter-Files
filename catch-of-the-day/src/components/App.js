ac7fd098ec9bd3acf3368a5fe1f8dfa2cc21c4f2
Kevin Candelaria <kevdashdev@gmail.com>
2019-10-08 14:53:12 +0800

    08 - Add base Router component

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,18 +3,14 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="catch-of-the-day">
-        <div className="menu">
-          <Header tagLine="Fresh Daily" />
-        </div>
-        <Order />
-        <Inventory />
-      </div>
-    );
-  }
-}
+const App = ({match}) => (
+  <div className="catch-of-the-day">
+    <div className="menu">
+      <Header tagLine={match.params.storeId} />
+    </div>
+    <Order />
+    <Inventory />
+  </div>
+);
 
 export default App;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { render } from 'react-dom';
+import {render} from 'react-dom';
 
-import App from './components/App';
-import StorePicker from './components/StorePicker';
-import "./css/style.css";
+import Router from './components/Router';
+import './css/style.css';
 
-render(<App />, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
