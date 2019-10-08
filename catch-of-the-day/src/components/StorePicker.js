13cfa4dfef4b5fecfb564631b0bf7f28df94632e
Kevin Candelaria <kevdashdev@gmail.com>
2019-10-08 15:18:57 +0800

    10 - Set fun name for store picker input

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
   render() {
     return (
       <form className="store-selector" action="">
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
-          <button type="submit">Visit Store →</button>
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store →</button>
       </form>
-    )
+    );
   }
 }
 
