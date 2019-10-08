77257b9d962363369b4924d0df9b0be0664114c3
Kevin Candelaria <kevdashdev@gmail.com>
2019-10-08 14:36:27 +0800

    08 - Refactor header into stateless functional component

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,23 +1,19 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="top">
-        <h1>
-          Catch
-          <span className="ofThe">
-            <span className="of">of</span>
-            <span className="the">the</span>
-          </span>
-          Day
-        </h1>
-        <h3 className="tagline">
-          <span>{this.props.tagLine}</span>
-        </h3>
-      </header>
-    );
-  }
-}
+const Header = ({tagLine}) => (
+  <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <span className="of">of</span>
+        <span className="the">the</span>
+      </span>
+      Day
+    </h1>
+    <h3 className="tagline">
+      <span>{tagLine}</span>
+    </h3>
+  </header>
+);
 
 export default Header;
