11d8f76cbe87662441c6358f0d42a935b79c125a
Kevin Candelaria <kevdashdev@gmail.com>
2019-10-08 14:29:46 +0800

    07 - Pass tagLine as prop to header

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagLine="Fresh Daily" />
         </div>
         <Order />
         <Inventory />
