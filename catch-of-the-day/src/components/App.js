ec0202708790e573b1e1a471bf0d96dfd15eddf5
Kevin Candelaria <kevdashdev@gmail.com>
2019-10-09 12:52:14 +0800

    14 - Allow loading of sample fishes

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   state = {
@@ -15,6 +16,9 @@ class App extends React.Component {
     fishes[key] = fish;
     this.setState({fishes});
   };
+  loadSampleFishes = () => {
+    this.setState({fishes: sampleFishes});
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -22,7 +26,10 @@ class App extends React.Component {
           <Header tagLine={this.props.match.params.storeId} />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
