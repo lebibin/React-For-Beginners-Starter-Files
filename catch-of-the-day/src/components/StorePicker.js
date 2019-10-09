ce85d8110c332f9aa47d7f1bb9055b8edfe06e82
Kevin Candelaria <kevdashdev@gmail.com>
2019-10-09 10:47:17 +0800

    11 - Add binding to store name input

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,13 +2,20 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  goToStore = e => {
+    e.preventDefault();
+    const storeId = this.myInput.current.value;
+    console.log(storeId);
+  };
   render() {
     return (
-      <form className="store-selector" action="">
+      <form className="store-selector" onSubmit={this.goToStore} action="">
         <h2>Please Enter A Store</h2>
         <input
           type="text"
           required
+          ref={this.myInput}
           placeholder="Store Name"
           defaultValue={getFunName()}
         />
